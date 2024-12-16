d0d2145c119c9c39a521f5f5d4f02593273009d1
Ashar Khan <asharmalik.khan@gmail.com>
2024-12-16 16:55:20 -0500

    added the 2 selenium driver options required to run driver in a docker container

--- src/main.py
+++ src/main.py
@@ -90,6 +90,8 @@ def get_driver():
     options = webdriver.ChromeOptions()
     options.add_argument('--headless')
     options.page_load_strategy = 'eager'
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
     driver = webdriver.Chrome(options=options)
     driver.set_page_load_timeout(20)  # change the page load strategy or timout
     return driver
